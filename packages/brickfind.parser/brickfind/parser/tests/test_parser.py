86ed53aa9e0e74649582fe91d643803c6e80d699
Λlisue <lambdalisue@hashnote.net>
2011-08-21 14:28:03 +0900

    initial

---
+++ packages/brickfind.parser/brickfind/parser/tests/test_parser.py
@@ -0,0 +1,68 @@
+# vim: set fileencoding=utf8:
+"""
+Unittest module of brickfind.parser
+
+AUTHOR:
+    lambdalisue[Ali su ae] (lambdalisue@hashnote.net)
+    
+Copyright:
+    Copyright 2011 Alisue allright reserved.
+
+License:
+    Licensed under the Apache License, Version 2.0 (the "License"); 
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unliss required by applicable law or agreed to in writing, software
+    distributed under the License is distrubuted on an "AS IS" BASICS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+__AUTHOR__ = "lambdalisue (lambdalisue@hashnote.net)"
+__VERSION__ = "0.1.0"
+from nose.tools import eq_
+import os.path
+from datetime import datetime
+from .. import parse
+
+def test_parse():
+    EXPECTED = dict(
+        name="BBa_B0034",
+        description="RBS (Elowitz 1999) -- defines RBS efficiency",
+        type="RBS",
+        status="available",
+        results="works",
+        nickname="",
+        rating=1,
+        url="http://partsregistry.org/Part:BBa_B0034",
+        publish_at=datetime.strptime("2003-01-31", "%Y-%m-%d"),
+        author="Vinay S Mahajan, Voichita D. Marinescu, Brian Chow, " + \
+            "Alexander      D Wissner-Gross and Peter Carr IAP, 2003.",
+        quality="confirmed",
+        sequences=(
+            "aaagaggagaaa",
+            ),
+        features=(
+            dict(
+                title="",
+                type="conserved",
+                direction="forward",
+                startpos=5,
+                endpos=8,
+                ),
+            ),
+        )
+    def _check(entry, key, value):
+        if isinstance(value, dict):
+            for subkey, subvalue in value.iteritems():
+                _check(entry[key], subkey, subvalue)
+        else:
+            eq_(entry[key], value)
+    xml = open(os.path.join(os.path.dirname(__file__), r"BBa_B0034.xml"))
+    res = parse(xml)
+    entry = res[0]
+    for key, value in EXPECTED.iteritems():
+        _check(entry, key, value)
---
+++ packages/brickfind.parser/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(name='brickfind.parser',
+      version=version,
+      description='',
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['brickfind'],
+      include_package_data=True,
+      test_suite='nose.collector',
+      test_requires=['Nose'],  
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
